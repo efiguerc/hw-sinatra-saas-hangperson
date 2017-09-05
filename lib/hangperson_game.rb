c6b63d7a69cf4e0a880bc048505941de4fbb79e3
Edgar Figueroa <efiguerc@gmail.com>
2017-09-05 19:25:04 +0000

    setup HangpersonGame object to make test become green

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,12 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -24,4 +28,38 @@ class HangpersonGame
     }
   end
 
+  def guess(letter)
+    raise ArgumentError, "nil argument to guess" if letter == nil
+    raise ArgumentError, "no letter provided to guess" if letter.empty?
+    raise ArgumentError, "not a valid letter to guess" unless letter =~ /^[a-zA-Z]$/
+    c = letter.downcase
+    return false if @guesses.include?(c) || @wrong_guesses.include?(c)
+    if @word.include? c
+      @guesses += c
+    else
+      @wrong_guesses += c
+    end
+    true
+  end
+  
+  def word_with_guesses
+    partial_word = ""
+    @word.each_char do |c|
+      if @guesses.include? c
+        partial_word += c
+      else
+        partial_word += '-'
+      end
+    end
+    partial_word
+  end
+  
+  def check_win_or_lose
+    return :lose if @wrong_guesses.size >= 7
+    if word_with_guesses.include? '-'
+      return :play
+    else
+      return :win
+    end
+  end
 end
